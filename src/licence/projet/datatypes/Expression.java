b8a2cecb7aab01b275757dbec80981f8d2514a52
SCHNEIDER-MAUNOURY leo <leo.schneider-maunoury@etu.u-paris.fr>
2020-12-21 17:16:49 +0100

    Revert "Merge branch 'extension3' into 'master'"
    This reverts merge request !2

--- src/licence/projet/datatypes/Expression.java
+++
@@ -1,8 +0,0 @@
-package licence.projet.datatypes;
-
-import java.util.ArrayList;
-import java.util.Stack;
-
-public interface Expression {
-    public double getValue(Stack<Double> stack, ArrayList<Double> hist);
-}
