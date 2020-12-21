68400dac574149ebb7ca48d24bd0f0f7be5d8674
schneide <leo.schneider-maunoury@etu.u-paris.fr>
2020-12-21 17:35:31 +0100

    First implementation of callbacks (hist and pile only)

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,10 +1,13 @@
-import licence.projet.datatypes.BopExpression;
+import licence.projet.datatypes.GeneralExpression;
 import licence.projet.datatypes.Expression;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserInterface {
 
@@ -24,16 +27,20 @@ public class UserInterface {
     public static void main(String[] args) {
         boolean readyToQuit = false;
         Stack<Double> stack = new Stack<>();
+        ArrayList<Double> hist = new ArrayList<>();
         String input;
 
         while (!readyToQuit) {
             input = getInput();
-            Expression lastExprEntered = new BopExpression(input);
-            System.out.println(lastExprEntered.getValue(stack));
-            
+
             if (input.equals("q") || input.equals("quit")) {
                 readyToQuit = true;
+                continue;
             }
+
+            Expression lastExprEntered = new GeneralExpression(input);
+            System.out.println(lastExprEntered.getValue(stack, hist));
+
         }
     }
 }
