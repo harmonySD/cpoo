4ec55a149a1de9449c4110a988083dfe825311d0
SCHNEIDER-MAUNOURY leo <leo.schneider-maunoury@etu.u-paris.fr>
2021-01-09 15:56:51 +0100

    Added doc to Callback

--- src/main/java/licence/projet/datatypes/Callback.java
+++ src/main/java/licence/projet/datatypes/Callback.java
@@ -5,5 +5,12 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public interface Callback {
+    /**
+     * Returns the element in lst at the position given by the attribute 'index'
+     * Adds this element in the stack and history.
+     * @param lst   Can either be the stack or the history
+     * @param stack The stack used by the whole program to store and use values
+     * @param hist  The history used by the whole program to store and load values
+    /*
     String callback(AbstractList<String> lst, Stack<String> stack, ArrayList<String> hist);
 }
