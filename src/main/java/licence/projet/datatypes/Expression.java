98644da82c379024695e8cc7bc93071b3651213e
Simon-Duchatel Harmony <sd.harmony@yahoo.fr>
2021-01-09 16:08:11 +0100

    Update Expression.java

--- src/main/java/licence/projet/datatypes/Expression.java
+++ src/main/java/licence/projet/datatypes/Expression.java
@@ -4,5 +4,6 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public interface Expression {
+    /** return value add in stack and history **/
     public String getValue(Stack<String> stack, ArrayList<String > hist);
 }
