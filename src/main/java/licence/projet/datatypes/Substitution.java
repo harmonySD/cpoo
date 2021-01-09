04f68e76346cdacf2623d8f901d635805b28931e
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2021-01-09 12:38:18 +0100

    verification si contient que des chiffres

--- src/main/java/licence/projet/datatypes/Substitution.java
+++ src/main/java/licence/projet/datatypes/Substitution.java
@@ -8,11 +8,14 @@ import java.util.regex.Pattern;
 
 public class Substitution implements Expression{
 	private boolean isAllReal(String l) {
-		
-		 String regex = "*[a-z]*";
-	     Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-	     Matcher matcher = pattern.matcher(l);
-	     return matcher.find();
+		final String esp= " ";
+		String mots[]=l.split(esp);
+		for(int i=0; i<mots.length;i++) {
+			if(mots[i].charAt(0)>='a' && mots[i].charAt(0)<='z') {
+				return false;
+			}
+		}
+		return true;
 	}
 	private String getOpe(String l) {
 		final String esp=" ";
@@ -37,11 +40,12 @@ public class Substitution implements Expression{
 		}catch(Exception e) {
 			if(l.length()>1) {
 				for(int i=0;i<l.length();i++) {
-					//regarder plutot avec des strings
 					if(l.charAt(i)==s.charAt(0)) {
 						l=l.substring(0,i)+in.substring(0,in.length())+l.substring(i+1);
+						System.out.println(isAllReal(l));
 						stack.push(l);
 						return l;
+						
 					}
 				}
 			}else {
